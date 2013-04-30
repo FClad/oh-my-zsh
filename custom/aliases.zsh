f947079cff76d969f06dd2d7a48b966b0e333448
Francois Clad <fclad@unistra.fr>
2013-04-30 16:29:15 +0200

    Added system specific aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,12 +1,17 @@
+
+# Load system specific aliases
+if [[ $OSTYPE == "linux-gnu" ]]; then
+	source $ZSH/custom/aliases.linux
+elif [[ ${OSTYPE//[0-9.]/} == "darwin" ]]; then
+	source $ZSH/custom/aliases.osx
+fi
+
 # Custom aliases
 
-alias ls="ls -G"
 alias la="ls -a"
 alias ll="ls -la"
 
 alias tmux="TERM=screen-256color-bce tmux"
-
-alias ntpforcesync="sudo ntpdate -u $(systemsetup -getnetworktimeserver|awk '{print $4}')"
 
 alias memgrind="valgrind --leak-check=full --track-origins=yes"
 
